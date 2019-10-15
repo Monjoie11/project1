1798b3ef46120d87261d073a77ab2ff5b7d14e4f
Monjoie11 <jeff.franken@gmail.com>
2019-10-15 13:28:48 -0400

    html icon created tomcat server created new workspace

---
+++ index/index.component.js
@@ -0,0 +1,9 @@
+// function load() {
+//   window.location.href = "../login/login.component.html";
+// }
+
+// load();
+
+window.onload = function() {
+  window.location.href = "../login/login.component.html";
+};
