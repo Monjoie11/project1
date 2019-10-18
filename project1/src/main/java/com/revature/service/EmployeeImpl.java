b8d4203f35382ed6cc55f17f4bd467c2672697b3
Monjoie11 <jeff.franken@gmail.com>
2019-10-17 20:49:53 -0400

    git fun

--- project1/src/main/java/com/revature/service/EmployeeImpl.java
+++ project1/src/main/java/com/revature/service/EmployeeImpl.java
@@ -1,12 +1,23 @@
 package com.revature.service;
 
+
+import com.revature.dao.EmployeeJDBC;
 import com.revature.pojos.Employee;
 
 public class EmployeeImpl implements EmployeeService {
+	
+	EmployeeJDBC employeeJDBC = new EmployeeJDBC();
 
 	@Override
 	public Employee loginEmployee(String email, String password) {
-		// TODO Auto-generated method stub
+		System.out.println("Attempted login with credentials: Username - " + email + " Password - " + password);
+		
+		Employee employee = employeeJDBC.getEmployee(email);
+		
+		if ((employee != null) && (employee.getPassword().equals(password))) {
+			return employee;
+		}
+		
 		return null;
 	}
 
